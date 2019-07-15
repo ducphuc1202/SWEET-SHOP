147c9a0b916d1af4422f9b80a51ffa2fcee53f6a
ducphuc1202 <38980066+ducphuc1202@users.noreply.github.com>
2019-07-15 22:47:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var arrayLoiChuc = ["Chúc bạn Huệ ngày càng xinh đẹp, dễ thương, sớm thành hoa khôi",
-                     "Sẽ luôn vui vẻ, hạnh phúc và yêu đời",
+var arrayLoiChuc = ["Chúc em ngày càng xinh đẹp, dễ thương, sớm thành hoa khôi",
+                     "Sẽ luôn vui vẻ, hạnh phúc và yêu đời, và sẽ càng tốt, nếu người làm em hạnh phúc là anh",
                      "Tuổi mới thành công trên con đường mình chọn, học giỏi hơn nữa",
                      "Sẽ nhận được nhiều điều tốt đẹp, mọi việc đều như mong muốn nhé",
-                     "Và nhớ là, có chuyện gì buồn, cần chia sẻ thì..... Just talk to me :v",
-                     "Hết lời chúc rồi, không biết chúc gì nữa. Hy vọng đây là món quà nho nhỏ dành tặng bạn Huệ trong ngày sinh nhật. Vì cũng không biết làm gì hơn. Mong là nó mang lại chút niềm vui nho nhỏ!!! Happy Birthday"];
+                     "Và nhớ là, bên cạnh em sẽ luôn có anh, thương em, yêu em và quan tâm em",
+										 "Và chúc em - sẽ là bạn gái của anh một ngày nào đó",
+                     "Hết lời chúc rồi, không biết chúc gì nữa. Hy vọng đây là món quà nho nhỏ dành tặng em trong ngày sinh nhật. Vì cũng không biết làm gì hơn. Mong là nó mang lại chút niềm vui nho nhỏ!!! Happy Birthday Em - Người con gái anh thương"];
 var i = 0;
 function loiChucNext() {
     document.getElementById("foryou").innerHTML = arrayLoiChuc[i];
     i++;
-    if(i === 6) {
+    if(i === 7) {
         i = 0;
     }
 }
